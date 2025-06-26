bdce6c0d00c1552572e3092f72f4ba55479dd464
Stephen Szpak <srszpak@gmail.com>
2025-06-26 13:49:23 -0500

    scope: config
    restrict runtime db config to prod

--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -1,17 +1,19 @@
 import Config
 
-database_url = System.get_env("DATABASE_URL") ||
-  "ecto://dashboard:abc123@localhost:5432/dashboard_dev"
+if config_env() == :prod do
+  database_url = System.get_env("DATABASE_URL") ||
+    "ecto://dashboard:abc123@localhost:5432/dashboard_prod"
 
-config :api, Dashboard.Repo,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: false
+  config :api, Dashboard.Repo,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    ssl: false
 
-config :api, DashboardWeb.Endpoint,
-  http: [ip: {0,0,0,0}, port: String.to_integer(System.get_env("PORT") || "4000")],
-  secret_key_base: System.get_env("SECRET_KEY_BASE") || "secret",
-  server: true
+  config :api, DashboardWeb.Endpoint,
+    http: [ip: {0,0,0,0}, port: String.to_integer(System.get_env("PORT") || "4000")],
+    secret_key_base: System.get_env("SECRET_KEY_BASE") || "secret",
+    server: true
+end
 
 config :api, Oban,
   repo: Dashboard.Repo,
